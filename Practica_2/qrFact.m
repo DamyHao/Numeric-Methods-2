193eb0b35689c724ab06cbf3f9a48fe52ca8424b
Damia <subextrry@gmail.com>
2020-03-11 12:09:40 +0100

    Comentaris

--- Practica_2/qrFact.m
+++ Practica_2/qrFact.m
@@ -1,5 +1,5 @@
 function UR = qrFact(A)
-    format long g;
+    % Factoritza la matriu A en UR, on la diagonal i per sobre es R i en els vectors u que queden per sota hi ha la informacio de les transfomracions Q aplicades.
     [n, m] = size(A);
     
     if (n > m)
@@ -15,7 +15,7 @@ function UR = qrFact(A)
         B = A(ii:n, ii:m); % Es la A petita
         x = B(:, 1); % Es la primera columna de la A petita
         tau = sign(x(1)) * norm(x);
-        gama = 1 + x(1) / tau;
+        gama = 1 + x(1) / tau; % En aquest cas gama feta com a teoria.
         u = [1; x(2:end) / (x(1) + tau)]; % Posem el punt i coma per fe que sigui columna. Posem el 1 perq el necesitem per multiplicar pero despres el treurem
         vAux = gama * u'* B;
         A(ii:n, ii:m) = B - u * vAux; A(ii+1:end,ii) = u(2:end);
