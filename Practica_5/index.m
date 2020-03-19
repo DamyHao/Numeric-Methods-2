5a85a953a8a2107bdb9a0457916ed7bcaccb2229
Damia <subextrry@gmail.com>
2020-03-19 18:09:50 +0100

    canvi de nom

--- Practica_5/index.m
+++ Practica_5/index.m
@@ -4,30 +4,10 @@
 %load(fullfile('..', 'Practica_1', 'PLU.m'))
 addpath("../Practica_1")
 
-A = eye(3);
-PLU(A);
 
-N = 2 * n - 1;
-A = zeros(N);
-%Ara ho fem com la practica 1:
-for i = 1:1:N
-    %Si es parell:
-    if (mod(i, 2) == 0)
-        A(i, i - 1) = 1;
-        A(i, i) = -1;
-        A(i, i + 1) = -1;
-        %La primera i la ultima definides posteriorment
-    elseif (i ~= 1 && i ~= N)
-        A(i, i - 1) = -R;
-        A(i, i) = 2 * R;
-        A(i, i + 1) = R;
-    end
 
-end
 
-A(1, 1) = 2 * R;
-A(1, 2) = R;
 
-v = [0 * (1:N - 2), -R, 3 * R];
-A(N, :) = v;
+f = @(x)([exp(-20 * (x(1) + 1/4).^2) + (sin(30 * x(1)) .* exp(-20 * (x(1) - 1/4).^2)) / 4; 1]);
 
+disp(f([1,2]))
